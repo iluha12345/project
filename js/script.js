d0ec436cb9048dc6bccb5365658a211300989f90
Illia <example@gmail.com>
2025-01-22 19:24:42 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let answers = [];
+
+answers[0]=prompt('What is your name?', '');
+answers[1]=prompt('What is your surname?', '');
+answers[2]=prompt('How old are you?', '');
+
+document.write(`His name is: ${answers[0]}, his surname is: ${answers[1]}, he is ${answers[2]} years old`);

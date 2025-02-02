49a0be1a9e78b9d87c32d83413a51f9655a7ae4b
Illia <example@gmail.com>
2025-02-02 15:13:32 +0100

    second part

--- js/script.js
+++ js/script.js
@@ -1,19 +1,35 @@
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели,','');
+let numberOfFilms = +prompt('How many films you are watchced,', '');
 
 const personalMovieDb = {
-    count: numberOfFilms,  
+    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?',''),
-      b = prompt('На сколько оцените его?',''),
-      c = prompt('Один из последних просмотренных фильмов?',''),
-      d = prompt('На сколько оцените его?','');
 
-personalMovieDb.movies[a] = b;
-personalMovieDb.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+
+    const a = prompt('One of the last watched films?', ''),
+        b = prompt('It is rate?', '');
+
+    if (a !== null && b !== null && a !== '' && b !== '' && a.length < 50) {
+        personalMovieDb.movies[a] = b;
+    } else {
+        i--;
+    }
+}
+
+if (personalMovieDb.count < 10) {
+    alert('You watched not many films');
+} else if (personalMovieDb.count >= 10 && personalMovieDb.count <= 30) {
+    alert('You are classic watcher');
+} else if (personalMovieDb.count > 50) {
+    alert('You are film MAN');
+} else {
+    alert('ERROR');
+}
 
 console.log(personalMovieDb);
